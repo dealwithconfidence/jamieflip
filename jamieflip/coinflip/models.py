b3dc1a212fe7f3a52f5d9f54427bc84644f70191
dealwithconfidence <robloxplayer5097@gmail.com>
2026-05-27 20:57:33 -0700

    move requirements

--- jamieflip/coinflip/models.py
+++ jamieflip/coinflip/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    balance = models.IntegerField(max_digits=12, default=100)
+    balance = models.IntegerField(default=100)
     daily_claimed = models.DateTimeField(null=True, blank=True)
 
 class Coinflip(models.Model):
