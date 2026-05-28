759833bc3e8faa02aa2592d55fa1b59340202df8
dealwithconfidence <robloxplayer5097@gmail.com>
2026-05-27 20:54:44 -0700

    move requirements

--- jamieflip/mysite/asgi.py
+++ jamieflip/mysite/asgi.py
@@ -1,21 +1,17 @@
-"""
-ASGI config for mysite project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
 import os
 import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import coinflip.routing
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-django.setup()
+
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(URLRouter(coinflip.routing.websocket_urlpatterns))
+    'websocket': AuthMiddlewareStack(
+        URLRouter(coinflip.routing.websocket_urlpatterns)
+    )
 })
